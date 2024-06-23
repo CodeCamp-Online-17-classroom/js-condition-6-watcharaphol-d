d776c1a7b315f4fe51d6929fdc626b78a646d81d
watcharaphol.d <dangpiem@hotmail.com>
2024-06-23 11:06:11 +0700

    Create logic to check input number

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // write code here
+let input = prompt("Enter input number:");
+if (input === "") alert("Please enter the input number");
+else if (!isNaN(+input)) {
+    if (+input % 2 === 0) alert(`${input} is Even number`);
+    else if (+input % 2 === 1) alert(`${input} is Odd number`);
+    else alert("Invalid");
+} else alert("Invalid, The recived input is not number");
